d9462d84b4f31d07a96b365d2558de34c309f26e
cpcwood <cpcwood@outlook.com>
2019-11-18 17:53:26 +0000

    Chapter 2 Complete

--- chapter2/quiz1/questions/question_2.rb
+++ chapter2/quiz1/questions/question_2.rb
@@ -1,2 +1,10 @@
 # Write a program that multiplies `TWELVE` by `FOURTEEN` by `SIXTEEN`
 # and `puts`es the result.
+
+#Define variables
+TWELVE = 12
+FOURTEEN = 14
+SIXTEEN = 16
+
+#puts calculation
+puts TWELVE * FOURTEEN * SIXTEEN
